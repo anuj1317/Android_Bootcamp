299e5aacdca0693384aec69a739d3ee684bf994f
anujsingh <anujsingh1317@gmail.com>
2021-09-21 15:02:02 +0530

    First Commit in Kotlin1 branch

--- app/src/main/java/com/example/android_bootcamp_anuj/MainActivity.kt
+++ app/src/main/java/com/example/android_bootcamp_anuj/MainActivity.kt
@@ -3,9 +3,12 @@ package com.example.android_bootcamp_anuj
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class MainActivity : AppCompatActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-    }
+fun main() {
+
+    println("Exercise date: 17/02/2021\n")
+
+    // Q1 Write a program to replace a substring inside a string with another string.
+    println("Q1")
+    replaceString()
+
 }
