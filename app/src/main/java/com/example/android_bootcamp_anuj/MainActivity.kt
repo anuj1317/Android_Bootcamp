12bd56188f67366cecb0a1d6ccea9905b3f1d118
anujsingh <anujsingh1317@gmail.com>
2021-09-29 14:09:46 +0530

    Added layout,module,flavours

--- app/src/main/java/com/example/android_bootcamp_anuj/MainActivity.kt
+++ app/src/main/java/com/example/android_bootcamp_anuj/MainActivity.kt
@@ -1,11 +1,19 @@
 package com.example.android_bootcamp_anuj
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val loginbutton = findViewById<Button>(R.id.login)
+        loginbutton.setOnClickListener {
+            val intent = Intent(this, homeactivity::class.java)
+           startActivity(intent)
+        }
     }
 }
---
+++ app/src/main/java/com/example/android_bootcamp_anuj/homeactivity.kt
@@ -0,0 +1,11 @@
+package com.example.android_bootcamp_anuj
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class homeactivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_homeactivity)
+    }
+}
