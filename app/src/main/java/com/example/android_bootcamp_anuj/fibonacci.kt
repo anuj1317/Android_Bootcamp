3bc4960725e1ae3dacbf1a0ff76e11347d2b236e
anujsingh <anujsingh1317@gmail.com>
2021-11-02 07:57:12 +0530

    added fibonacci

--- app/src/main/java/com/example/android_bootcamp_anuj/fibonacci.kt
+++ app/src/main/java/com/example/android_bootcamp_anuj/fibonacci.kt
@@ -3,25 +3,25 @@ package com.example.android_bootcamp_anuj
 import kotlinx.coroutines.GlobalScope
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
-//
-//fun main() {
-//    GlobalScope.launch {
-//        printFibonacciSeries(15) // print after delay
-//    }
-//    println("Fibonacci series of 5 is:") // main thread continues while coroutine is delayed
-//    Thread.sleep(10000) // block main thread for 10 seconds to keep JVM alive
-//}
-//
-////suspend fun printFibonacciSeries(endRange: Int) {
-////    var t1 = 0; var t2 = 1; var nextTerm = 0;
-////    for (i in 1..endRange) {
-////        print(" $t1")
-////        nextTerm = t1 + t2;
-////        t1 = t2;
-////        t2 = nextTerm;
-////        delay(300)
-////    }
-////}
+
+fun main() {
+    GlobalScope.launch {
+        printFibonacciSeries(15) // print after delay
+    }
+    println("Fibonacci series of 5 is:") // main thread continues while coroutine is delayed
+    Thread.sleep(10000) // block main thread for 10 seconds to keep JVM alive
+}
+
+suspend fun printFibonacciSeries(endRange: Int) {
+    var t1 = 0; var t2 = 1; var nextTerm = 0;
+    for (i in 1..endRange) {
+        print(" $t1")
+        nextTerm = t1 + t2;
+        t1 = t2;
+        t2 = nextTerm;
+        delay(300)
+    }
+}
 
 
 
